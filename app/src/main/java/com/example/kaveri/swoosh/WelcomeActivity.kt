5aecf2dc8013f1c9eb7a0957b45a51d6562bfcf3
Kaveri <kaveriphi915@gmail.com>
2017-11-12 17:42:02 +0530

    Included the Skill screen layout

--- app/src/main/java/com/example/kaveri/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/example/kaveri/swoosh/WelcomeActivity.kt
@@ -3,9 +3,10 @@ package com.example.kaveri.swoosh
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
